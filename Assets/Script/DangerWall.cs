7561217bd24c01c25328c1a381abdf086982f7bf
tkhr-saito <tkhr.saito@gmail.com>
2019-05-07 14:19:23 +0900

    Obstable and Reset others:version up Unity2017 to Unity2018

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+    void OnCollisionEnter(Collision hit)
+    {
+        //playerに接触したら
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            //現在のシーン番号を取得
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            //現在のシーンを再読込する
+            SceneManager.LoadScene(sceneIndex);
+
+        }
+
+    }
+}
