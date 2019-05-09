cc2036ba9f018db49437df8e72e1611ceb327485
tkhr-saito <tkhr.saito@gmail.com>
2019-05-09 11:27:45 +0900

    Do it my self (Finished)

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
     public Text scoreLabel;
+    public Text timeLabel;
     public GameObject winnerLabelObject;
+    public GameObject loserLabelObject;
+    public GameObject player;
+
+    //持ち時間の初期値はとりあえず60で（GameControllerで指定するっぽい？）
+    public float time = 60;
+
 
 	// Use this for initialization
 	void Start () {
@@ -26,5 +33,19 @@ public class GameController : MonoBehaviour {
         }
 
 
-	}
+        //timeが0になったらGAMEOVER表示
+        if (time < 0.0)
+        {
+            //オブジェクトをアクティブにして、ボールを消して、カウントを止める
+            loserLabelObject.SetActive(true);
+            Destroy(player);
+            time = 0;
+        }
+        else
+        {
+            //毎秒持ち時間表示減らす
+            timeLabel.text = time.ToString();
+            time = time - Time.deltaTime;
+        }
+    }
 }
